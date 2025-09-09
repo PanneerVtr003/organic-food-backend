8ec333b6bc45e137ce6539ddb50a412447f79246
PanneerVtr003 <panneerselvam944393@gmail.com>
2025-09-09 11:22:14 +0530

    added

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -4,6 +4,11 @@ import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+// Test endpoint
+router.get("/test", (req, res) => {
+  res.json({ message: "Orders endpoint is working!" });
+});
+
 router.post("/", protect, addOrderItems);
 
 export default router;
